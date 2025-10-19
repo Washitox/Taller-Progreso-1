4ca7bcc93999a0bd78347245976177aa3932aa8e
Washington <117743091+Washitox@users.noreply.github.com>
2025-10-18 22:25:04 -0500

    Update MensajeYoda.java

--- Ejercicios del 4 al 6/Ejercicio5_MensajeYoda/src/MensajeYoda.java
+++ Ejercicios del 4 al 6/Ejercicio5_MensajeYoda/src/MensajeYoda.java
@@ -1,15 +1,38 @@
 import java.util.Scanner;
+import java.util.Stack;
 
 public class MensajeYoda {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
         System.out.println("Ingrese la secuencia RPN de Yoda (ej: \"entrenar\" \"debes\" + \"camino\" \"el\" + +):");
         String linea = sc.nextLine();
 
+        // Dividimos los tokens por espacio
         String[] tokens = linea.split(" ");
-        String mensaje = RPN.reconstruirMensaje(tokens);
 
-        System.out.println("Mensaje de Yoda: " + mensaje);
+        Stack<String> pila = new Stack<>();
+
+        for (String token : tokens) {
+            if (token.equals("+")) {
+                if (pila.size() >= 2) {
+                    String second = pila.pop(); // último ingresado
+                    String first = pila.pop();  // penúltimo ingresado
+                    // Concatenar el último delante del penúltimo para respetar el orden RPN
+                    pila.push(second + " " + first);
+                } else {
+                    System.out.println("Error: operador '+' sin suficientes operandos.");
+                }
+            } else {
+                // Eliminamos las comillas si existen
+                token = token.replaceAll("^\"|\"$", "");
+                pila.push(token);
+            }
+        }
+
+        if (pila.size() == 1) {
+            System.out.println("Mensaje de Yoda: " + pila.pop());
+        } else {
+            System.out.println("Error: La expresión RPN no está bien formada.");
+        }
     }
 }
